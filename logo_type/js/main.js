288e347ca542b6066154f0ee0b504c606988ffbf
muslimevlo <muslimevlo@yandex.ru>
2020-02-03 17:16:17 +0300

    bondi,goserf

---
+++ logo_type/js/main.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    $('.slider__inner, .news__slider-inner').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',  
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        infinitie: false
+    });
+    $('select').styler();
+
+    $('.header__btn-menu').on('click', function(){
+    $('.menu ul').slideToggle();
+    });
+
+});
